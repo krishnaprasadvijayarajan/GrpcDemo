038400f11ac44dafd3c6e38ae29cfdb87d61e0f0
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-02-20 20:23:07 +0000

    Updates

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -9,11 +9,11 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var input = new HelloRequest {Name="Krishna" };
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(input);
-            Console.WriteLine(reply.Message);
+            //var input = new HelloRequest {Name="Krishna" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(input);
+            //Console.WriteLine(reply.Message);
 
             Console.ReadLine();
         }
